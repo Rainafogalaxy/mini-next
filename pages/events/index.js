04063aac33c4bf2243f7f166d5f5edf1e33aad9a
Azi <yuwuyinhe@163.com>
2023-09-22 21:13:02 +0800

    add some images

---
+++ pages/events/index.js
@@ -0,0 +1,14 @@
+//tip :
+// 像这样，建立一个文件夹后，里面的index.js就相当于直接在外侧建立一个events.js
+
+// This is the page for a single event
+
+function AllEventsPage() {
+  return (
+    <div>
+      <h1>All Events</h1>
+    </div>
+  );
+}
+
+export default AllEventsPage;
