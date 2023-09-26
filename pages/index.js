44ee01fea98e2451cf29577fd54c8b48c796c8f6
Azi <yuwuyinhe@163.com>
2023-09-26 18:54:32 +0800

    new

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,16 @@ function HomePage() {
   );
 }
 
+// 在首页使用预渲染
+
+export async function getStaticProps() {
+  return {
+    props: {
+      featuredEvents:
+    },
+  };
+}
+
 export default HomePage;
 
 //在哪里创建小组件都可以，但是不应该创建在pages文件夹下，在pages文件夹下，只应该包含page组件，否则nextjs就会把它们当做页面生成路径
